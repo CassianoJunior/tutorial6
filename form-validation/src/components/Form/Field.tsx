0c480b5507e87061524f4660915c6cd0443d8e3c
Cassiano Junior <cassianojuniorww@gmail.com>
2023-06-05 15:38:12 -0300

    feat(FormComponents): Add form components

---
+++ form-validation/src/components/Form/Field.tsx
@@ -0,0 +1,11 @@
+import { HTMLAttributes } from "react";
+
+interface FieldProps extends HTMLAttributes<HTMLDivElement> {}
+
+const Field = (props: FieldProps) => {
+  return (
+    <div className="flex flex-col gap-1" {...props} />
+  )
+}
+
+export { Field };
---
+++ form-validation/src/components/Form/Label.tsx
@@ -0,0 +1,12 @@
+import { LabelHTMLAttributes } from "react";
+
+const Label = (props: LabelHTMLAttributes<HTMLLabelElement>) => {
+  return (
+    <label 
+      className="text-sm text-zinc-600 flex items-center justify-between"
+      {...props}
+    />
+  )
+}
+
+export { Label };
